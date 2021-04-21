49186bb143f1c236d91b31055969f9779e56d4ac
Sorin95 <sorin.dobre@s.unibuc.ro>
2021-04-21 08:29:39 +0300

    v4

--- app/src/main/java/com/example/Connnect4/Game.java
+++ app/src/main/java/com/example/Connnect4/Game.java
@@ -9,14 +9,22 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.google.android.material.snackbar.Snackbar;
+
 public class Game extends Fragment {
 
-//    int[][] mat = { { 0, 0, 0, 0, 0, 0, 0 },
-//                    { 0, 0, 0, 0, 0, 0, 0 },
-//                    { 0, 0, 0, 0, 0, 0, 0 },
-//                    { 0, 0, 0, 0, 0, 0, 0 },
-//                    { 0, 0, 0, 0, 0, 0, 0 },
-//                    { 0, 0, 0, 0, 0, 0, 1 },};
+    int[][] mat = { { 0, 0, 0, 0, 0, 0, 0 },
+                    { 0, 0, 0, 0, 0, 0, 0 },
+                    { 0, 0, 0, 0, 0, 0, 0 },
+                    { 0, 0, 0, 0, 0, 0, 0 },
+                    { 0, 0, 0, 0, 0, 0, 0 },
+                    { 0, 0, 0, 0, 0, 0, 0 },};
+
+    Boolean player1 = true;
+    int gamestate = 1;
+    Boolean draw = false;
+    int value = 0; //pentru players
+    int victory = 1;
 
     @Override
     public View onCreateView(
@@ -37,5 +45,91 @@ public class Game extends Fragment {
                         .navigate(R.id.action_Game_to_Menu);
             }
         });
+    }
+
+    public void onClick(View view) {
+        if (checkWin() == 1) {
+            Snackbar.make(view, "Player 1 wins!", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+        }
+        if (checkWin() == 2) {
+            Snackbar.make(view, "Player 2 wins!", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+        }
+        if (checkWin() == 3) {
+            Snackbar.make(view, "Draw!", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+        }
+//           if (checkWin() == 4) {
+//               //aici trebuia sa dea valori matricei
+//           }
+
+    }
+
+    @NonNull
+    public int checkWin() {
+        draw = true;
+        value = 0;
+        if (horizontalCheck() || verticalCheck() || ascendingDiagonalCheck() || descendingDiagonalCheck()) {
+            return value == victory ? 1 : 2;
+        }
+        // nobody won, return mDraw if it is, nothing if it's not
+        return draw ? 3 : 4;
+    }
+
+    private boolean horizontalCheck() {
+        // orizontal
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < 6 - 3; j++) {
+                value = mat[i][j];
+                if (value == 0) draw = false;
+                if (value != 0 && mat[i][j + 1] == value && mat[i][j + 2] == value && mat[i][j + 3] == value) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean verticalCheck() {
+        // vertical
+        for (int j = 0; j < 7; j++) {
+            for (int i = 0; i < 6 - 3; i++) {
+                value = mat[i][j];
+                if (value == 0) draw = false;
+                if (value != 0 && mat[i + 1][j] == value && mat[i + 2][j] == value && mat[i + 3][j] == value) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean ascendingDiagonalCheck() {
+        // ca un fel de diagonala secundara
+        for (int i = 3; i < 7; i++) {
+            for (int j = 0; j < 6 - 3; j++) {
+                value = mat[i][j];
+                if (value == 0) draw = false;
+                if (value != 0 && mat[i - 1][j + 1] == value && mat[i - 2][j + 2] == value && mat[i - 3][j + 3] == value) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean descendingDiagonalCheck() {
+        // diagonala principala sor of
+        for (int i = 3; i < 7; i++) {
+            for (int j = 3; j < 6; j++) {
+                value = mat[i][j];
+                if (value == 0) draw = false;
+                if (value != 0 && mat[i - 1][j - 1] == value && mat[i - 2][j - 2] == value && mat[i - 3][j - 3] == value) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
